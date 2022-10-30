cf8396d98441d8778318d27358ff68c09d6ea724
monccciii <44330082+monccciii@users.noreply.github.com>
2022-10-30 18:34:10 -0400

    all my content so far
    - missing clearer dir instructions
    - needs a function for moving vids into folder (alr does it but not clean code)
    - better gui design needed

---
+++ ytdownloader/main.py
@@ -0,0 +1,38 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from mainwindow import Ui_MainWindow
+from pytube import YouTube
+import os.path
+import shutil
+import time
+
+save_path = '/Downloads/'
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(lambda: self.begin_search())
+
+    def begin_search(self):
+        link = self.lineEdit.text()
+        print('good')
+        yt = YouTube(link)
+
+        print(yt.title, yt.views)
+        try:
+             
+            yd = yt.streams.get_highest_resolution()
+            yd.download()
+            src = f'{yt.title}.mp4'
+            dst = 'Downloads'
+            shutil.move(src, dst)
+            self.label_8.setText("Your video has been downloaded, Congrats!")
+            self.label_9.setText("Please check the Downloads directory.")
+        except:
+            self.label_8.setText('An error occurred. Please try again.')
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
